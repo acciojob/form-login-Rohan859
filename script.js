fa9a637806134baf4143856544ef78992a135cf3
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-16 21:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	let inputs=document.querySelectorAll("input");
+	let firstName=inputs[0].value;
+	let lastName=inputs[1].value;
+	let fullName=firstName+" "+lastName;
+	alert(fullName);
 }
